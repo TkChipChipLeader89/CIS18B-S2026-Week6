5d004e44a782d9d7fa870efb91064405b339260f
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-04-15 04:39:59 +0000

    Completed mini 7 Challenge

--- week6-exercises/mini07-generic-abstract-factory-capstone/src/main/java/edu/norcocollege/cis18b/week6/mini07/GenericAbstractFactoryDemo.java
+++ week6-exercises/mini07-generic-abstract-factory-capstone/src/main/java/edu/norcocollege/cis18b/week6/mini07/GenericAbstractFactoryDemo.java
@@ -5,8 +5,10 @@ public class GenericAbstractFactoryDemo {
     public static void main(String[] args) {
         String campusLine = ScreenRenderer.render(new CampusUIFactory());
         String mobileLine = ScreenRenderer.render(new MobileUIFactory());
+        String infoLine = ScreenRenderer.render(new InfoUIFactory());
 
         System.out.println("Campus UI: " + campusLine);
         System.out.println("Mobile UI: " + mobileLine);
+        System.out.println("Info UI: " + infoLine);
     }
 }
---
+++ week6-exercises/mini07-generic-abstract-factory-capstone/src/main/java/edu/norcocollege/cis18b/week6/mini07/InfoButton.java
@@ -0,0 +1,9 @@
+package edu.norcocollege.cis18b.week6.mini07;
+
+//InfoButton class
+public class InfoButton implements Button {
+    @Override
+    public String render() {
+        return "InfoButton";
+    }
+}
